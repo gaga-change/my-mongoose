2cb7103c70beb65e54bae94fececaeadad7629bf
严俊东 <1172482914@qq.com>
2017-10-11 18:38:26 +0800

    页面采用bootstrap

---
+++ 02login/static/js/common.js
@@ -0,0 +1,25 @@
+/**
+ * Created by yanjd on 2017/10/11.
+ */
+(function () {
+  var con = $(`<div style="position: fixed; left: 0; bottom: 0"></div>`)
+  $('body').append(con)
+  /**
+   * 内部提示，两秒后消失
+   * @param msg
+   */
+  window.myAlert = function (msg) {
+    console.log('???')
+    var template = $(`
+      <div class="column">
+      <div class="alert alert-dismissable alert-warning">
+      <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+      ${msg}
+      </div>
+      </div>`)
+    con.append(template)
+    setTimeout(function () {
+      template.remove()
+    }, 3000)
+  }
+})()
