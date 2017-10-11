b9944107f74fdf0735a72597332d736d62d6ef65
严俊东 <1172482914@qq.com>
2017-10-11 14:34:55 +0800

    代码标准化

--- 02login/router.js
+++ 02login/router.js
@@ -3,11 +3,11 @@ const github = require('./db/github.js')
 
 module.exports = function (app, router) {
   /* 用户 User */
-  router.use('/', user.session)                   // 获取session存储的用户
-  router.post('/login', user.login)           // 登入接口
-  router.post('/register', user.register)     // 注册接口
-  router.get('/getAccount', user.getAccount)  // 获取当前登入账号接口
-  router.get('/logout', user.logout)          // 退出登入接口
+  router.use('/', user.session) // 获取session存储的用户
+  router.post('/login', user.login) // 登入接口
+  router.post('/register', user.register) // 注册接口
+  router.get('/getAccount', user.getAccount) // 获取当前登入账号接口
+  router.get('/logout', user.logout) // 退出登入接口
 
   /* 第三方登入 */
   router.get('/login/github', github.githubLogin)
