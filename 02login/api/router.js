03e9fc5ad45ea375155ee0035354397989d05a59
严俊东 <1172482914@qq.com>
2017-10-12 19:40:16 +0800

    添加链接接口

--- 02login/api/router.js
+++ 02login/api/router.js
@@ -1,5 +1,6 @@
 const user = require('./db/user.js')
 const github = require('./db/github.js')
+const address = require('./db/address.js')
 
 module.exports = function (app, router) {
   /* 用户 User */
@@ -10,8 +11,11 @@ module.exports = function (app, router) {
   router.get('/logout', user.logout) // 退出登入接口
 
   /* 第三方登入 */
-  router.get('/login/github', github.githubLogin)
-  router.get('/login/github/callback', github.githubCallback)
+  router.get('/login/github', github.githubLogin) // github 第三方登入跳转地址
+  router.get('/login/github/callback', github.githubCallback) // github登入回调地址
 
+  /* 收藏 */
+  router.post('/address/add', address.add)
+  // 配置接口前缀
   app.use('/api', router)
 }
