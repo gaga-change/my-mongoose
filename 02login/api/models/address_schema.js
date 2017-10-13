ebb155293414576b529e57ef10a8911653de5fdb
严俊东 <1172482914@qq.com>
2017-10-13 10:02:57 +0800

    数据结构变动

--- 02login/api/models/address_schema.js
+++ 02login/api/models/address_schema.js
@@ -6,9 +6,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const AddressSchema = new Schema({
+  user: {type: Schema.ObjectId, ref: 'User'},
+  share: {type: Boolean, default: false},
   title: {type: String, default: ''},
   detail: {type: String, default: ''},
-  url: {type: String, default: ''}
+  url: {type: String, default: ''},
+  time: {
+    create: {type: Date, default: Date.now()},
+    modify: {type: Date, default: Date.now()}
+  }
 })
 
 AddressSchema.methods = {}
--- 02login/api/models/collect_schema.js
+++ 02login/api/models/collect_schema.js
@@ -6,10 +6,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const CollectSchema = new Schema({
-  // user: { type : Schema.ObjectId, ref : 'User' },
-  arr: {
-    user: {type: Schema.ObjectId, ref: 'Address'}
-  }
+  user: {type: Schema.ObjectId, ref: 'User'},
+  private: [{
+    fileName: {type: String, default: ''},
+    url: {type: Schema.objectId, ref: 'Url'}
+  }],
+  grade: {type: Object, default: ''},
+  share: [{
+    url: {type: Schema.objectId, ref: 'Url'}
+  }]
 })
 
 module.exports = mongoose.model('Collect', CollectSchema)
