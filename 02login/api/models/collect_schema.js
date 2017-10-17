057ff41a6d1c299798b9d6ba48dbed948036225b
严俊东 <1172482914@qq.com>
2017-10-17 19:33:06 +0800

    修改

--- 02login/api/models/collect_schema.js
+++ 02login/api/models/collect_schema.js
@@ -14,4 +14,15 @@ const CollectSchema = new Schema({
   grade: {type: Object, default: ''}
 })
 
+/* 实例方法 */
+CollectSchema.methods = {}
+
+/* 静态方法 */
+CollectSchema.statics = {
+  // 根据用户ID获取 收藏夹的目录
+  findGradeByUserId (userId) {
+
+  }
+}
+
 module.exports = mongoose.model('Collect', CollectSchema)
