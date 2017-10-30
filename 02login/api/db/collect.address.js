d5bdf0028d0382ac03cf895d9c1b40bf434efd69
严俊东 <1172482914@qq.com>
2017-10-30 11:10:21 +0800

    移动链接到其它目录

--- 02login/api/db/collect.address.js
+++ 02login/api/db/collect.address.js
@@ -64,7 +64,56 @@ exports.delete = function (req, res, next) {
 
 /**
  * 修改收藏链接
+ * 单个操作：
+ *    修改内容 （id title detail url)
+ *    修改目录 (id fileName)
+ * 多个操作
+ *    修改目录（id fileName）
  */
+
+exports.put = function (req, res) {
+  let params = only(req.body, 'id title detail url fileName')
+  if (!params.id || params.id.toString().length === 0) {
+    res.send({success: false, message: 'id 值不能为空'})
+  } else if (params.fileName)  { // 修改目录操作
+    if (params.id.split(',').length === 1) { // 单条操作
+      Collect.update({user: req.user, 'collect.fileName' : params.fileName}, {collect: {$eleMatch: {fileName: params.fileName}}}).then(msg => {
+        
+      })
+    } else { // 多条操作
+
+    }
+  } else { // 修改链接相关参数
+    Address.update({id: params.id}, {title: params.title, detail: params.detail, url: params.url})
+  }
+  Object.keys(params).map(key => {
+
+  })
+  let adrs = only(params, 'title detail url')
+  if (params.fileName) {}
+  adrs = new Address(adrs)
+  adrs.user = req.user
+  adrs.save().then((address) => {
+    Collect.update({user: req.user, grade: params.fileName}, {
+      $push: {
+        collect: {
+          fileName: params.fileName,
+          address
+        }
+      }
+    }).then((msg) => {
+      if (msg.nModified) {
+        res.send({success: true})
+      } else {
+        adrs.remove()
+        res.send({success: false, message: '目录名不存在'})
+      }
+    })
+  }).catch((err) => {
+    const errors = Object.keys(err.errors).map(key => err.errors[key].message)
+    res.send({success: false, message: errors[0], errors})
+  })
+}
 
 /**
  * 查询收藏链接
