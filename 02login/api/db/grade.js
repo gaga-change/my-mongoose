f6744709b141b113966329d76624627f2e02f9d6
严俊东 <1172482914@qq.com>
2017-11-03 17:40:29 +0800

    目录增删改查接口改动

--- 02login/api/db/grade.js
+++ 02login/api/db/grade.js
@@ -14,7 +14,6 @@ exports.add = function (req, res) {
   let gradeName = params.name
   if (!gradeName) return res.send({success: false, message: '目录名不能为空'})
   Grade.update({user: req.user, name: gradeName}, {}, {upsert: true}).then(msg => {
-    console.log(msg)
     if (msg.upserted) {
       res.send({success: true})
     } else {
@@ -27,21 +26,9 @@ exports.add = function (req, res) {
  * 删除目录
  */
 exports.delete = function (req, res) {
-  let gradeNames = req.body.gradeNames
-  try {
-    gradeNames = JSON.parse(gradeNames)
-  } catch (err) {
-    return res.send({success: false, message: '参数不是JSON数组'})
-  }
-  if (gradeNames.constructor !== Array) return res.send({success: false, message: '参数需为数组'})
-  if (!gradeNames.length) return res.send({success: false, message: '目录名不能为空'})
-  console.log(gradeNames)
-  Grade.update({user: req.user}, {$pullAll: {grade: gradeNames}}, {upsert: true}).select('grade').then(msg => {
-    if (msg.nModified) {
-      res.send({success: true})
-    } else {
-      res.send({success: false, message: '目录名不存在'})
-    }
+  const params = only(req.body, 'id')
+  Grade.remove({_id: params.id}).then(() => {
+    res.send({success: true})
   })
 }
 
@@ -49,21 +36,9 @@ exports.delete = function (req, res) {
  * 重命名目录
  */
 exports.modify = function (req, res) {
-  let oldGradeName = req.body.oldGradeName || ''
-  let newGradeName = req.body.newGradeName || ''
-  if (oldGradeName === newGradeName) return res.send({success: false, message: '目录名相同'})
-  if (!oldGradeName || !newGradeName) return res.send({success: false, message: '目录名不能为空'})
-  Grade.findOne({user: req.user, grade: newGradeName}).select('grade').then(Grade => {
-    return !!Grade
-  }).then((exist) => {
-    if (exist) return res.send({success: false, message: '新目录名已存在'})
-    Grade.update({user: req.user, grade: oldGradeName}, {$set: {'grade.$': newGradeName}}).select('grade').then(msg => {
-      if (msg.nModified) {
-        res.send({success: true})
-      } else {
-        res.send({success: false, message: '目录名不存在'})
-      }
-    })
+  const params = only(req.body, 'id name')
+  Grade.update({_id: params.id}, {name: params.name}).then((msg) => {
+    res.send({success: true, msg})
   })
 }
 
